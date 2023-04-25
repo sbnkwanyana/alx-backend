635a4539a8130b0bd9ed043abcc34f03486d23c9
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-04-25 14:47:37 +0200

    1. Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Simple flask web app with extension that works with translations
+"""
+
+
+from flask_babel import Babel
+from flask import Flask, render_template
+from config import Config
+
+app = Flask(__name__)
+
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """
+    Returns a simple html page
+    """
+    return render_template("1-index.html")
+
+if __name__ == "__main__":
+    """
+    Starts the flask app
+    """
+    app.run()
+
---
+++ 0x02-i18n/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+"""
+
+
+class Config(object):
+    """
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
