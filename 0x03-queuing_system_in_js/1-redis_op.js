12db866f47c0eed46cd02acd376ea82b5537e105
Simphiwe Nkwanyana <sbnkwanyana@hotmail.com>
2023-05-24 09:13:39 +0200

    2. Node Redis client and basic operations

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,14 +1,12 @@
 import { createClient, print } from 'redis';
 
+
 const client = createClient();
 
 client.on('error', (error) => {
     console.log('Redis client not connected to the server:', error)
 });
 
-client.on('connect', () => {
-    console.log('Redis client connected to the server')
-});
 
 const setNewSchool = (schoolName, value) => {
     client.set(schoolName, value, print)
@@ -19,6 +17,10 @@ const displaySchoolValue = (schoolName) => {
         console.log(value)
     })
 };
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server')
+});
 
 displaySchoolValue('Holberton');
 setNewSchool('HolbertonSanFrancisco', '100');
